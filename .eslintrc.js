454747ce82ca9b0a04bc73a3e1647144677c2ae8
Stephen S <webstacker@outlook.com>
2020-03-19 00:35:15 +0000

    Update eslint settings and fix lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,21 @@ module.exports = {
     env: {
         node: true
     },
+    // overwrite airbnb-base to use commonjs instead of ES6 modules
+    parserOptions: {
+        ecmaVersion: 9,
+        sourceType: 'script',
+        ecmaFeatures: {
+            modules: false
+        }
+    },
     rules: {
         'prettier/prettier': ['error'],
         'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
         'curly': ['error', 'all'],
-        'jest/expect-expect': ['error']
+        'jest/expect-expect': ['error'],
+        // https://github.com/eslint/eslint/issues/8953#issuecomment-317697474
+        strict: ['error', 'safe']
     },
     plugins: ['prettier']
 };
